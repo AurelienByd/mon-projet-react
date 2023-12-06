a211a43243786899c86419bc44379b84beff8350
Aurélien <rubis91999@hotmail.com>
2023-12-06 14:06:45 +0100

    application api

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import './App.css';
 import Exercice1 from './Exercice1.jsx';
 import Exercice2 from './Exercice2.jsx';
 import Exercice3 from './Exercice3.jsx';
+import Api from './ExerciceApiThemoviedb.jsx';
 
 export default function App() {
+
   return (
     <>
-      <Exercice1 />
-      <Exercice2 />
-      <Exercice3 />
+      <Api />
     </>
   );
+  
+  // return (
+  //   <>
+  //     <Exercice1 />
+  //     <Exercice2 />
+  //     <Exercice3 />
+  //   </>
+  // );
 }
---
+++ src/ExerciceApiThemoviedb.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react"
+
+export default function Api() {
+    const [text, setText] = useState('')
+    const [movies, setMovies] = useState([])
+
+    const fetchMoviesData = () => {
+        fetch("http://api.themoviedb.org/3/search/movie?api_key=f33cd318f5135dba306176c13104506a&query="+text)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                setMovies(data.results)
+            })
+    }
+
+    return (
+        <>
+            <input
+                type='text'
+                value={text}
+                onChange={event => setText(event.target.value)}
+            />
+            <button
+                onClick={fetchMoviesData}
+            >Ajouter</button>
+            {movies.map(movie => (
+                <li>{movie.title} ({movie.release_date})</li>
+            ))}
+        </>
+    )
+}
